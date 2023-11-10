4d700a7e672dca84c38b2697ef853ab6246e6d89
EFREN NARVAEZ <efrennm86@gmail.com>
2023-11-10 13:16:29 -0600

    Proyecto API Tiendas Postgresql, Express, Knex

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// Importar las rutas de mi vista (Home)
+//const homeRoutes = require('./routes/homeRoutes')
+
+const app = express()
+
+/* Middlewares */
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+/* Routes */
+//app.use('/api/v1', homeRoutes)
+
+/* Levantar el servidor */
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000')
+})
